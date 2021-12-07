1f00dd53c80866f469282534bce503ff7320afc1
renovat10 <amalk.cuj@gmail.com>
2021-12-07 06:55:56 +0530

    Update Threesum.py

--- Threesum.py
+++ Threesum.py
@@ -1,6 +1,15 @@
 import sys #For using CLI arguments. Change to argparse in future versions
 
-def count(a):
+def printAll(a): #Prints all triplets with sum 0
+    N = len(a)
+    count = 0
+    for i in range(N):
+        for j in range(i+1, N):
+            for k in range(j+1, N):
+                if a[i]+a[j]+a[k] == 0:
+                    print(str(a[i]) + " " + str(a[j]) + " " + str(a[k])
+
+def count(a): #Returns count of triplets with sum 0
     N = len(a)
     count = 0
     for i in range(N):
